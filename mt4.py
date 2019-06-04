ac1721e562041abfa9f7e5fdfc72560b797baa49
koswag <44467051+koswag@users.noreply.github.com>
2019-06-04 18:04:24 +0200

    Update mt4.py

--- mt4.py
+++ mt4.py
@@ -1,66 +1,107 @@
+from trading import Strategy
 import zmq
 
-class Manager:
-    """Uses ZMQ Connection to manage client's requests"""
 
-    con = None
+class Trader:
+    def __init__(self, push, pull):
+        self.connection = Connection()
+        self.connection.connect(push, pull)
+        self.strategy = Strategy('H1', 10)
+        self.Tickets = []
 
-    def __init__(self, zmqConnection):
-        self.con = zmqConnection
-    
-    def getRates(self, symbol):
-        """Gives rates for given symbol. Format: BID|ASK"""
-        req = self.con.reqSocket
-        pull = self.con.pullSocket
+    def rates(self, symbol):
         query = "RATES|{}".format(symbol)
-        self.con.send(req, query)
-        res = self.con.pull(pull)
-        return res
+        self.connection.push(query)
 
-    def sendOrder(self, action, orderType, symbol):
-        """Sends an order for specified symbol. action = <'OPEN' or 'CLOSE'>; orderType = <0 for BUY or 1 for SELL>"""
-        req = self.con.reqSocket
-        query = "TRADE|{}|{}|{}|0|50|50|Python-to-MT4".format(action, orderType, symbol)
-        self.con.send(req, query)
+    def data(self, symbol, time_frame):
+        query = "DATA|{}|{}".format(symbol, time_frame)
+        self.connection.push(query)
+
+    def order(self, action, order_type, symbol, tick_id=None):
+        query = 'TRADE|{}|{}|{}|0|50|50|Python-to-MT4'.format(action, order_type, symbol)
+        if tick_id:
+            query += "|{}".format(tick_id)
+        self.connection.push(query)
+    
+    def push_listener(self):
+        while True:
+            msg = str(self.connection.pull())
+            if msg is None or msg == "":     
+                continue
+            self.process(msg)
+
+    def process(self, msg):
+        print "Processing message : {}".format(msg)
+        values = parse(msg)
+
+        if values[0] == "TRADE":
+            print values[1]
+            self.add_ticket(values[2])
+        elif values[0] == "RATES":
+            bid, ask = values[1], values[2]
+            # do sth with bid and ask
+        elif values[0] == "DATA":
+            op, cl, high, low = values[1], values[2], values[3], values[4]
+            self.strategy.add_bar(op, cl, high, low)
+
+    def add_ticket(self, tick):
+        try:
+            ticket = int(tick)
+            if ticket > 0:
+                self.Tickets.append(ticket)
+        except ValueError:
+            pass
+
 
 class Connection:
-    """Provides functionality to connect to ZMQ MT4 Server"""
-
-    context     = None
-    reqSocket   = None
-    pullSocket  = None
-    reqPort     = None
-    pullPort    = None
-
-    def __init__(self, reqPort, pullPort):
-        self.context    = zmq.Context()
-        self.reqSocket  = self.context.socket(zmq.REQ)
+    def __init__(self):
+        self.context = zmq.Context()
+        self.pushSocket = self.context.socket(zmq.PUSH)
         self.pullSocket = self.context.socket(zmq.PULL)
-        self.reqPort    = reqPort
-        self.pullPort   = pullPort
 
-    def connect(self):
-        """Connects client to the server on given request and pull ports."""
-        self.reqSocket.connect("tcp://localhost:{}".format(self.reqPort))
-        self.pullSocket.connect("tcp://localhost:{}".format(self.pullPort))
-        print "Connected to server on ports: {} and {}".format(self.reqPort, self.pullPort)
+    def connect(self, pushPort, pullPort):
+        self.pushSocket.connect("tcp://localhost:{}".format(pushPort))
+        self.pullSocket.connect("tcp://localhost:{}".format(pullPort))
+        print "Connected to ports: {} and {}".format(pushPort, pullPort)
 
-    def send(self, socket, data):
-        """Tries to send a data message through given socket."""
+    def disconnect(self):
+        "Terminates the connection."
+        print "Disconnecting push socket.."
+        self.pushSocket.close()
+        print "Done"
+        
+        print "Disconnecting pull socket.."
+        self.pullSocket.close()
+        print "Done"
 
+    def push(self, msg):
+        "Sends a string message."
+        self.pushSocket.send_string(unicode(msg))
+
+    def pull(self):
+        """Tries to retrieve a message.
+        
+        Returns message as byte-string if there's any in the buffer."""
+        pull = self.pullSocket
+        
         try:
-            socket.send(data)
-            msg = socket.recv_string()
-            print msg
-            
-        except zmq.Again:
-            print "Waiting for PUSH from MetaTrader 4.."
-    
-    def pull(self, socket):
-        """Tries to pull a message from given socket."""
-        try:
-            msg = socket.recv(flags=zmq.NOBLOCK)
+            msg = pull.recv(flags=zmq.NOBLOCK)
             return msg
-            
         except zmq.Again:
-            print "Waiting for PUSH from MetaTrader 4.."
+            return None
+
+
+def parse(msg, sep='|'):
+    components = msg.split(sep)
+    vals = []
+
+    for val in components:
+        try:
+            vals.append(int(val))
+        except ValueError:
+            try:
+                vals.append(float(val))
+            except ValueError:
+                vals.append(val)
+    
+    return vals
